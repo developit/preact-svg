62a8e6aa3918f52a19dfdefc167d9126f541e925
Jason Miller <jasonmiller@synacor.com>
2015-11-30 07:56:51 -0500

    Patch all properties with attribute mapping, on all SVG nodes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import { h, Component } from 'preact';
 
 const DOM = typeof document!=='undefined' && !!document.createElement;
 
-/** A list of SVG properties that are read-only, requiring `setAttribute()` to mutate.
- *	@private
- */
-const ATTRS = DOM ? ['width', 'height', 'viewBox', 'preserveAspectRatio'] : [];
+const PROP_TO_ATTR_MAP = {
+	'className': 'class'
+}
 
 
 /** During <SVG> DOM building, this flag is set to `true`.
@@ -15,13 +14,21 @@ let updateMode = false;
 
 
 if (DOM) {
+	let div = document.createElement('div');
+
 	/** Hook into document.createElement() to slap a namespace on manually namespaced nodes (yup)
 	 *	@private
 	 */
 	let oldCreate = document.createElement;
 	document.createElement = name => {
 		if (updateMode || name==='svg') {
-			return document.createElementNS('http://www.w3.org/2000/svg', name);
+			let el = document.createElementNS('http://www.w3.org/2000/svg', name);
+			for (let i in el) {
+				if (!(i in div) || PROP_TO_ATTR_MAP.hasOwnProperty(i)) {
+					Object.defineProperty(el, i, contentPropertyDef(i));
+				}
+			}
+			return el;
 		}
 		return oldCreate.call(document, name);
 	};
@@ -37,12 +44,12 @@ let memoize = (fn, mem={}) => k => mem.hasOwnProperty(k) ? mem[k] : (mem[k] = fn
 /** Create a getter/setter pair for a Content Property that proxies to the corresponding attribute.
  *	@private
  */
-let contentPropertyDef = memoize( prop => ({
+let contentPropertyDef = memoize( (prop, attr=(PROP_TO_ATTR_MAP[prop] || prop)) => ({
 	set(v) {
-		if (v===null || v===undefined) this.removeAttribute(prop);
-		else this.setAttribute(prop, v);
+		if (v===null || v===undefined) this.removeAttribute(attr);
+		else this.setAttribute(attr, v);
 	},
-	get() { return this.getAttribute(prop); }
+	get() { return this.getAttribute(attr); }
 }) );
 
 
@@ -57,16 +64,6 @@ export default class SVG extends Component {
 	// after any update, manually apply exempted readOnly SVG properties as attributes:
 	componentDidUpdate() {
 		updateMode = false;
-		let { base, props } = this;
-		for (let i in props) {
-			if (props.hasOwnProperty(i) && ~ATTRS.indexOf(i) && base.getAttribute(i)!==props[i]) {
-				base.setAttribute(i, props[i]);
-				if (!base['_psvg'+i]) {
-					base['_psvg'+i] = true;
-					Object.defineProperty(base, i, contentPropertyDef(i));
-				}
-			}
-		}
 	}
 
 	render({ children, ...props }) {
@@ -76,13 +73,6 @@ export default class SVG extends Component {
 			this.hasRendered = updateMode = true;
 		}
 
-		let attrs = {
-			xmlns: 'http://www.w3.org/2000/svg',
-			version: '1.1'
-		};
-		for (let i in props) if (props.hasOwnProperty(i) && ATTRS.indexOf(i)<0) {
-			attrs[i] = props[i];
-		}
-		return <svg {...attrs}>{ children }</svg>;
+		return <svg version="1.1" xmlns="http://www.w3.org/2000/svg" {...props}>{ children }</svg>;
 	}
 }
